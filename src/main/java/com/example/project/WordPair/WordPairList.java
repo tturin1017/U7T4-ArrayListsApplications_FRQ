42bfc28addeb4bd77c383784d9c4e526d5f49fc7
Tatiana Turin <146445767+tturin1017@users.noreply.github.com>
2025-02-06 17:23:12 +0000

    final

--- src/main/java/com/example/project/WordPair/WordPairList.java
+++ src/main/java/com/example/project/WordPair/WordPairList.java
@@ -7,13 +7,6 @@ public class WordPairList {
 
     public WordPairList(String[] words) {
         /* to be implemented in part (a) */
-        allPairs = new ArrayList<WordPair>();
-        for(int i =0 ; i<words.length-1;i++){
-            for(int j=i+1; j<words.length;j++){
-                WordPair pair = new WordPair(words[i], words[j]);
-                allPairs.add(pair);
-            }
-        }
     }
 
     // The method below was added for testing;
@@ -24,13 +17,7 @@ public class WordPairList {
 
     public int numMatches() {
         /* to be implemented in part (b) */
-        int count = 0;
-        for(WordPair pair: allPairs){
-            if(pair.getFirst().equals(pair.getSecond())){
-                count++;
-            }
-        }
-        return count;
+        return 0;
     }
 
     // The method below was added for testing;
